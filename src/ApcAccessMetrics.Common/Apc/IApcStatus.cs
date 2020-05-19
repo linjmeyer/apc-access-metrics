fee182e04f268141babc2ddab5915e1865b7363f
Lin Meyer <lin@linmeyer.net>
2020-05-19 00:33:21 -0500

    Fixed typo, added unit tests

---
+++ src/ApcAccessMetrics.Common/Apc/IApcStatus.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApcAccessMetrcs.Common.Apc
+{
+    /// <summary>
+    /// Status of an APC
+    /// </summary>
+    public interface IApcStatus
+    {
+        
+    }
+}
